a7f06f4ae84361cbea2e30825c756fa3bf869d49
abubakar sabo <abkundi001@gmail.com>
2022-10-28 13:06:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { ProfileSelection } from './components/ProfileSelection';
+import { TwitterButton } from './components/TwitterButton';
+import { ButtonZuri } from './components/ButtonZuri';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ProfileSelection />
+    <div className="container">
+      <TwitterButton />
+      <ButtonZuri />
+    </div>
+    
     </div>
   );
 }
---
+++ src/components/TwitterButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TwitterButton = () => {
+  return (
+    <div id='twitter__1'>
+        <h3><a href="http://twitter.com/tariqYA_">Twitter Link</a></h3>
+    </div>
+  )
+}
