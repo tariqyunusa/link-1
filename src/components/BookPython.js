490b4e49baf6255de3e16bae050e4865fb7339d1
abubakar sabo <abkundi001@gmail.com>
2022-10-28 23:32:34 +0100

    init

---
+++ src/components/BookPython.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BookPython = () => {
+  return (
+    <div id='book__python'>
+        <h3><a href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>">Zuri Team</a></h3>
+    </div>
+  )
+}
