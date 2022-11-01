1c44c61356a15c2310f64832f088df22ca86bfae
tariq yunusa <abkundi001@gmail.com>
2022-11-01 15:20:58 +0100

    twelveth commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
     <div className='footer'>
         <div className='container-2'>
             <div className='content'>
-                <img src="/zuri.internship_Logo.jpg" />
+                <img src="/Zuri.internship_Logo.jpg" />
             <div className='footer__text'>
               <h3>HNG Internship 9 Frontend Task</h3>
             </div>
--- src/components/SocialSection.js
+++ src/components/SocialSection.js
@@ -4,7 +4,7 @@ export const SocialSection = () => {
   return (
     <div className='social__section' id='social__section'>
         <img src="/slack.jpg"  />
-        <img src="/social icon.jpg" className='social-icon' />
+        <img src="/Social icon.jpg" className='social-icon' />
     </div>
   )
 }
