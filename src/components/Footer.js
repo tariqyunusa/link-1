f404527cd454a5c92723e2e53ba3db15a2bc764e
abubakar sabo <abkundi001@gmail.com>
2022-11-01 08:30:22 +0100

    seventh commit

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='container-2'>
+            <div className='content'>
+                <img src="zuri.internship_Logo.jpg" style={{top:'32px',
+            position:'relative'}} />
+            <div className='footer__text'>
+                
+            </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
